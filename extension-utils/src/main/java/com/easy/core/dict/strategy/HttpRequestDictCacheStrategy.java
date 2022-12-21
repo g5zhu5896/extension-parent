da0e93f5f7314da4905d7c15ef4a92aa521cef51
zhuzhenxiong <haoxiangshui12@qq.com>
2022-12-21 22:02:32 +0800

    一次请求时在service中另起一个线程调用查询进行字段转换时报错问题处理

--- extension-utils/src/main/java/com/easy/core/dict/strategy/HttpRequestDictCacheStrategy.java
+++ extension-utils/src/main/java/com/easy/core/dict/strategy/HttpRequestDictCacheStrategy.java
@@ -17,21 +17,34 @@ public class HttpRequestDictCacheStrategy implements DictCacheStrategy {
     @Override
     public IDictBean cacheDictBean(IDictBean dictBean, String cacheKey) {
         //将dictBean放入缓存
-        getCache().put(cacheKey, dictBean);
+        Map<String, IDictBean> cache = getCache();
+        if (cache != null) {
+            cache.put(cacheKey, dictBean);
+        }
         return dictBean;
     }
 
     @Override
     public IDictBean getFromCache(String cacheKey) {
-        return getCache().get(cacheKey);
+        Map<String, IDictBean> cache = getCache();
+        if (cache != null) {
+            return cache.get(cacheKey);
+        } else {
+            return null;
+        }
     }
 
     private Map<String, IDictBean> getCache() {
-        Object dictCache = RequestContextHolder.getRequestAttributes().getAttribute("dictCache", RequestAttributes.SCOPE_REQUEST);
-        if (dictCache == null) {
-            dictCache = new HashMap<String, IDictBean>();
-            RequestContextHolder.getRequestAttributes().setAttribute("dictCache", dictCache, RequestAttributes.SCOPE_REQUEST);
+        RequestAttributes requestAttributes = RequestContextHolder.getRequestAttributes();
+        if (requestAttributes != null) {
+            Object dictCache = requestAttributes.getAttribute("dictCache", RequestAttributes.SCOPE_REQUEST);
+            if (dictCache == null) {
+                dictCache = new HashMap<String, IDictBean>();
+                requestAttributes.setAttribute("dictCache", dictCache, RequestAttributes.SCOPE_REQUEST);
+            }
+            return (Map<String, IDictBean>) dictCache;
+        } else {
+            return null;
         }
-        return (Map<String, IDictBean>) dictCache;
     }
 }
